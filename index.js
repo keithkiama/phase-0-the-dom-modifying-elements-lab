a4b60bf865f999010847152589ac62f6de1d9967
keithkiama <keithkiama2003@gmail.com>
2022-12-02 18:31:18 +0300

    Completed Assignement

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ const newHeader = document.createElement('h1');
 
 newHeader.setAttribute("id", "victory");
 newHeader.textContent = "Keith is the champion";
+
+// const ul = document.createElement("ul");
+
+// adding an element to a list
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+// element.append(ul);
+
+// li.textContent = "Hi there!";
+// console.log(li.textContent);
